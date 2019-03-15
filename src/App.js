fec0bb7a70995894de4ab835b6b889004fabb382
Matthieu <matthieunadeau96@gmail.com>
2019-03-15 14:06:35 -0400

    Added a components directory and created skeletons for VideoPreview and Home

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import HeaderNav from './containers/HeaderNav/HeaderNav';
 import { SideBar } from './containers/SideBar/SideBar';
+import { Home } from './containers/Home/Home';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <React.Fragment>
         <HeaderNav/>
         <SideBar/>
+        <Home/>
       </React.Fragment>
     );
   }
---
+++ src/containers/Home/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {VideoPreview} from '../../components/VideoPreview/VideoPreview'
+
+export class Home extends React.Component {
+  render() {
+    return (
+      <VideoPreview/>
+    )
+  }
+}

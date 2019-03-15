9dfe3fe4f2ae3243d2411125d6b38f55fc0631c0
Matthieu <matthieunadeau96@gmail.com>
2019-03-15 14:09:06 -0400

    Adjusted the home styling

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import {VideoPreview} from '../../components/VideoPreview/VideoPreview'
+import './Home.scss'
 
 export class Home extends React.Component {
   render() {
     return (
-      <VideoPreview/>
+      <div className="home">
+        <VideoPreview/>
+      </div>
     )
   }
 }
